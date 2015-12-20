a54c0f5da893feefe99adcce9885a399b1db6c4e
YurovaA <yurova92@gmail.com>
2015-12-20 14:46:46 +0100

    fixed algorithm in cannon_skew; latest version of async (with an old algorithm) in cannon_async

--- cannon_src/cannon_skew.c
+++ cannon_src/cannon_skew.c
@@ -40,7 +40,7 @@ int main (int argc, char **argv) {
 	// create a 2D cartesian grid 
 	dimensions[0] = dimensions[1] = sqrt_size;
 	periods[0] = periods[1] = 1;    
-	MPI_Cart_create(MPI_COMM_WORLD, 2, dimensions, periods, 1, &cartesian_grid_communicator);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dimensions, periods, 0, &cartesian_grid_communicator);
 	MPI_Cart_coords(cartesian_grid_communicator, rank, 2, coordinates);
 
 	// create a row communicator
@@ -58,6 +58,7 @@ int main (int argc, char **argv) {
 	if (rank == 0){
 		int row, column;
 		if ((fp = fopen (argv[1], "r")) != NULL){
+			printf("Reading!");
 			fscanf(fp, "%d %d\n", &matrices_a_b_dimensions[0], &matrices_a_b_dimensions[1]);
 			A = (double **) malloc (matrices_a_b_dimensions[0] * sizeof(double *));
 			for (row = 0; row < matrices_a_b_dimensions[0]; row++){
@@ -181,9 +182,9 @@ int main (int argc, char **argv) {
 	if(rank == 0) {
 		int i;
 		for(i = 1; i < size; i++){
-			printf("A: %d \t %d \n", i, adjust_i_A(i, sqrt(size)));
-			MPI_Send((A_array + (i * A_local_block_size)), A_local_block_size, MPI_DOUBLE, adjust_i_A(i, sqrt(size)), 0, cartesian_grid_communicator);
-			printf("B: %d \t %d \n", i, adjust_i_B(i, sqrt(size)));
+			//printf("A: %d \t %d \n", i, adjust_i_A(i, sqrt(size)));
+			MPI_Send((A_array + (i * A_local_block_size)), A_local_block_size, MPI_DOUBLE, adjust_i_A(i, sqrt(size)), 1, cartesian_grid_communicator);
+			//printf("B: %d \t %d \n", i, adjust_i_B(i, sqrt(size)));
 			MPI_Send((B_array + (i * B_local_block_size)), B_local_block_size, MPI_DOUBLE, adjust_i_B(i, sqrt(size)), 0, cartesian_grid_communicator);
 		}
 		for(i = 0; i < A_local_block_size; i++){
@@ -193,16 +194,47 @@ int main (int argc, char **argv) {
 			B_local_block[i] = B_array[i];
 		}
 	} else {
-		MPI_Recv(A_local_block, A_local_block_size, MPI_DOUBLE, 0, 0, cartesian_grid_communicator, &status);
+		MPI_Recv(A_local_block, A_local_block_size, MPI_DOUBLE, 0, 1, cartesian_grid_communicator, &status);
 		MPI_Recv(B_local_block, B_local_block_size, MPI_DOUBLE, 0, 0, cartesian_grid_communicator, &status);
 	}
-	return 1;
+	//return 1;
 	// cannon's algorithm
 	int cannon_block_cycle;
 	double compute_time = 0, mpi_time = 0, start;
 	int C_index, A_row, A_column, B_column;
+	/*if (rank == 7){
+		int i, j, k, row, column;
+		printf("Local matrices!\n");
+		for(row = 0; row < A_local_block_rows; row++) {
+				for(column = 0; column < A_local_block_columns; column++)
+					printf ("%7.3f ", A_local_block[row * A_local_block_columns + column]);
+				printf ("\n");
+			}
+		for(row = 0; row < B_local_block_rows; row++) {
+				for(column = 0; column < B_local_block_columns; column++)
+					printf ("%7.3f ", B_local_block[row * B_local_block_columns + column]);
+				printf ("\n");
+			}
+	}*/
+	//if (rank == 0) printf("Reached Cannon!");
 	for(cannon_block_cycle = 0; cannon_block_cycle < sqrt_size; cannon_block_cycle++){
 		// compute partial result for this block cycle
+		/*if (rank == 7){
+		printf("Iteration number: %7.3f\n", cannon_block_cycle);
+		int i, j, k, row, column;
+		printf("Local matrices!\n");
+		for(row = 0; row < A_local_block_rows; row++) {
+				for(column = 0; column < A_local_block_columns; column++)
+					printf ("%7.3f ", A_local_block[row * A_local_block_columns + column]);
+				printf ("\n");
+			}
+		printf("\n");
+		for(row = 0; row < B_local_block_rows; row++) {
+				for(column = 0; column < B_local_block_columns; column++)
+					printf ("%7.3f ", B_local_block[row * B_local_block_columns + column]);
+				printf ("\n");
+			}
+	}*/
 		start = MPI_Wtime();
 		for(C_index = 0, A_row = 0; A_row < A_local_block_rows; A_row++){
 			for(B_column = 0; B_column < B_local_block_columns; B_column++, C_index++){
@@ -212,6 +244,15 @@ int main (int argc, char **argv) {
 				}
 			}
 		}
+		/*if (rank == 7){
+		int i, j, k, row, column;*/
+		/*printf("C!\n");
+		for(row = 0; row < A_local_block_rows; row++) {
+				for(column = 0; column < B_local_block_columns; column++)
+					printf ("%7.3f ", C_local_block[row * B_local_block_columns + column]);
+				printf ("\n");
+			}*/
+	//}
 		compute_time += MPI_Wtime() - start;
 		start = MPI_Wtime();
 		// rotate blocks horizontally
@@ -253,31 +294,32 @@ int main (int argc, char **argv) {
 					}
 				}
 			}
+			
 		}
 
-		printf("(%d,%d)x(%d,%d)=(%d,%d)\n", A_rows, A_columns, B_rows, B_columns, A_rows, B_columns);
-		printf("Computation time: %lf\n", compute_time);
-		printf("MPI time:         %lf\n", mpi_time);
+		//printf("(%d,%d)x(%d,%d)=(%d,%d)\n", A_rows, A_columns, B_rows, B_columns, A_rows, B_columns);
+		//printf("Computation time: %lf\n", compute_time);
+		//printf("MPI time:         %lf\n", mpi_time);
 
 		if (argc == 4){
 			// present results on the screen
-			printf("\nA( %d x %d ):\n", A_rows, A_columns);
+			//printf("\nA( %d x %d ):\n", A_rows, A_columns);
 			for(row = 0; row < A_rows; row++) {
-				for(column = 0; column < A_columns; column++)
-					printf ("%7.3f ", A[row][column]);
-				printf ("\n");
+			//	for(column = 0; column < A_columns; column++)
+				//	printf ("%7.3f ", A[row][column]);
+				//printf ("\n");
 			}
-			printf("\nB( %d x %d ):\n", B_rows, B_columns);
+			//printf("\nB( %d x %d ):\n", B_rows, B_columns);
 			for(row = 0; row < B_rows; row++){
-				for(column = 0; column < B_columns; column++)
-					printf("%7.3f ", B[row][column]);
-				printf("\n");
+				//for(column = 0; column < B_columns; column++)
+				//	printf("%7.3f ", B[row][column]);
+				//printf("\n");
 			}
-			printf("\nC( %d x %d ) = AxB:\n", A_rows, B_columns);
+			//printf("\nC( %d x %d ) = AxB:\n", A_rows, B_columns);
 			for(row = 0; row < A_rows; row++){
-				for(column = 0; column < B_columns; column++)
-					printf("%7.3f ",C[row][column]);
-				printf("\n");
+				//for(column = 0; column < B_columns; column++)
+				//	printf("%7.3f ",C[row][column]);
+				//printf("\n");
 			}
 
 
@@ -291,12 +333,12 @@ int main (int argc, char **argv) {
 					for(k=0; k<B_rows; k++){
 						temp += A[i][k] * B[k][j];
 					}
-					printf("%7.3f ", temp);
+				//	printf("%7.3f ", temp);
 					if(temp != C[i][j]){
 						pass = 0;
 					}
 				}
-				printf("\n");
+				//printf("\n");
 			}
 			if (pass) printf("Consistency check: PASS\n");
 			else printf("Consistency check: FAIL\n");
